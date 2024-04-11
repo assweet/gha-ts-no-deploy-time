970f1ec6c1e460794cdced62f4ef503457c75c7d
Lim Swee Tat <slim@abnormalsecurity.com>
2024-04-11 23:24:33 +0800

    Adding the correct checks and associated tests

---
+++ __tests__/check.test.ts
@@ -0,0 +1,99 @@
+import {
+  checkHolidays,
+  checkOfficeHours,
+  checkWeekend,
+  convertDateTz
+} from '../src/check'
+import { expect } from '@jest/globals'
+
+describe('check.ts', () => {
+  // checkHoliday
+  it('checkHoliday with valid holiday', async () => {
+    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const result: boolean = checkHolidays(date, 'SG', 'SG', 'SG')
+
+    expect(result).toBeTruthy()
+  })
+  it('checkHoliday with invalid holiday', async () => {
+    const date: Date = new Date('2024-04-15T12:00:00.0800')
+    const result: boolean = checkHolidays(date, 'SG', 'SG', 'SG')
+
+    expect(result).toBeFalsy()
+  })
+  // checkOfficeHours
+  it('checkOfficeHours with valid time', async () => {
+    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const nowHour: number = date.getHours()
+    const result: boolean = checkOfficeHours(nowHour, '9', '22')
+
+    expect(result).toBeTruthy()
+  })
+  it('checkOfficeHours with after office hours', async () => {
+    const date: Date = new Date('2024-04-10T23:00:00.0800')
+    const nowHour: number = date.getHours()
+    const result: boolean = checkOfficeHours(nowHour, '9', '22')
+
+    expect(result).toBeFalsy()
+  })
+  it('checkOfficeHours with before office hours', async () => {
+    const date: Date = new Date('2024-04-10T04:00:00.0800')
+    const nowHour: number = date.getHours()
+    const result: boolean = checkOfficeHours(nowHour, '9', '22')
+
+    expect(result).toBeFalsy()
+  })
+  it('checkOfficeHours with just at end office hours', async () => {
+    const date: Date = new Date('2024-04-10T22:00:00.0800')
+    const nowHour: number = date.getHours()
+    const result: boolean = checkOfficeHours(nowHour, '9', '22')
+
+    expect(result).toBeFalsy()
+  })
+  // checkWeekend
+  it('checkWeekend with weekday', async () => {
+    const date: Date = new Date('2024-04-10T22:00:00.0800')
+    const dayOfWeek: string = date.toLocaleDateString('en-US', {
+      weekday: 'long'
+    })
+    const noDeploymentDays = 'Friday,Saturday,Sunday'
+    const result: boolean = checkWeekend(dayOfWeek, noDeploymentDays)
+
+    expect(result).toBeFalsy()
+  })
+  it('checkWeekend with weekend', async () => {
+    const date: Date = new Date('2024-04-12T22:00:00.0800')
+    const dayOfWeek: string = date.toLocaleDateString('en-US', {
+      timeZone: 'America/Los_Angeles',
+      weekday: 'long'
+    })
+    const noDeploymentDays = 'Friday,Saturday,Sunday'
+    const result: boolean = checkWeekend(dayOfWeek, noDeploymentDays)
+
+    expect(result).toBeTruthy()
+  })
+  // convertTZ
+  it('convertDateTz with valid timezone', async () => {
+    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const [resultDate, resultHour, resultDayOfWeek] = convertDateTz(date, '+8')
+
+    expect(resultDate.getTime()).toEqual(date.getTime())
+    expect(resultHour).toEqual(12)
+    expect(resultDayOfWeek.includes('wednesday')).toBeTruthy()
+  })
+  it('convertDateTz with different timezone', async () => {
+    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const [resultDate, resultHour, resultDayOfWeek] = convertDateTz(date, '-5')
+
+    expect(resultDate.getTime()).toBeLessThan(date.getTime())
+    expect(resultHour).toEqual(23)
+    expect(resultDayOfWeek.includes('tuesday')).toBeTruthy()
+  })
+  it('convertDateTz with invalid offset', async () => {
+    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const t = (): boolean => {
+      convertDateTz(date, 'abc')
+      return true
+    }
+    expect(t).toThrow(TypeError)
+  })
+})
---
+++ src/check.ts
@@ -0,0 +1,111 @@
+import Holidays from 'date-holidays'
+
+/**
+ * The convertDateTz cnnvert the date to the specified timezone
+ * @returns {Date}` Resolves when the action is complete.
+ */
+export function convertDateTz(
+  today: Date,
+  offset: string
+): [Date, number, string] {
+  // Some data validation before continuing
+  if (offset === '') {
+    throw new TypeError('Timezone offset not specified')
+  }
+  const offsetInt: number = parseInt(offset, 10) * 3600000
+  if (isNaN(offsetInt)) {
+    throw new TypeError('Timezone offset invalid')
+  }
+  const todayTime: number = today.getTime()
+  const localOffset: number = today.getTimezoneOffset() * 60000
+  const utcTime: number = todayTime + localOffset
+  const resultTime: number = utcTime + offsetInt
+  const resultDate: Date = new Date(resultTime)
+
+  const resultHour: number = resultDate.getHours()
+  const weekday: string[] = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+  ]
+  const resultDayOfWeekInt: number = resultDate.getDay()
+  const resultDayOfWeek: string = weekday[resultDayOfWeekInt]
+
+  return [resultDate, resultHour, resultDayOfWeek]
+}
+
+/**
+ * The checkWeekend checks if the date time is within week days
+ * @returns {boolean} Resolves when the action is complete.
+ */
+export function checkWeekend(
+  dayOfWeek: string,
+  noDeploymentDays: string
+): boolean {
+  // Some data validation before continuing
+  if (dayOfWeek === '') {
+    throw new TypeError('dayOfWeek not properly defined')
+  }
+  if (noDeploymentDays === '') {
+    throw new TypeError('noDeploymentDays is not properly defined')
+  }
+  const noDeploymentDaysCommas = `,${noDeploymentDays.toLowerCase()},`
+  const result: boolean = noDeploymentDaysCommas.includes(
+    `,${dayOfWeek.toLowerCase()},`
+  )
+  return result
+}
+
+/**
+ * The checkOfficeHours checks if the date time is within office hours
+ * @returns {boolean} Resolves when the action is complete.
+ */
+export function checkOfficeHours(
+  nowHour: number,
+  officeHoursStart: string,
+  officeHoursEnd: string
+): boolean {
+  // Some data validation before continuing
+  if (officeHoursStart === '' || officeHoursEnd === '') {
+    throw new TypeError('Either office hours start or end is not defined')
+  }
+  const officeHoursStartInt: number = parseInt(officeHoursStart, 10)
+  const officeHoursEndInt: number = parseInt(officeHoursEnd, 10)
+
+  if (
+    isNaN(nowHour) ||
+    isNaN(officeHoursStartInt) ||
+    isNaN(officeHoursEndInt)
+  ) {
+    throw new TypeError(
+      'Either nowHour or office hours start or end is not a number'
+    )
+  }
+  if (nowHour <= officeHoursStartInt || nowHour >= officeHoursEndInt) {
+    return false
+  }
+  return true
+}
+
+/**
+ * The checkHolidays checks if the date time is correct
+ * @returns {boolean}` Resolves when the action is complete.
+ */
+export function checkHolidays(
+  tzToday: Date,
+  country: string,
+  state: string,
+  region: string
+): boolean {
+  // Some data validation before continuing
+  if (!(tzToday instanceof Date) || isNaN(tzToday.getTime())) {
+    throw new TypeError('today is not a date')
+  }
+  const h = new Holidays(country, state, region)
+  const isTodayHoliday = h.isHoliday(tzToday)
+  return isTodayHoliday !== false
+}
