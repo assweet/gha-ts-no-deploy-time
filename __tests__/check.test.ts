eb4a366231f6a7e4a92404539b78be20ef46fa31
Lim Swee Tat <slim@abnormalsecurity.com>
2024-04-11 23:39:31 +0800

    Fixing the date string for tests - github action timezone different

--- __tests__/check.test.ts
+++ __tests__/check.test.ts
@@ -9,41 +9,41 @@ import { expect } from '@jest/globals'
 describe('check.ts', () => {
   // checkHoliday
   it('checkHoliday with valid holiday', async () => {
-    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const date: Date = new Date('2024-04-10T12:00:00+0800')
     const result: boolean = checkHolidays(date, 'SG', 'SG', 'SG')
 
     expect(result).toBeTruthy()
   })
   it('checkHoliday with invalid holiday', async () => {
-    const date: Date = new Date('2024-04-15T12:00:00.0800')
+    const date: Date = new Date('2024-04-15T12:00:00+0800')
     const result: boolean = checkHolidays(date, 'SG', 'SG', 'SG')
 
     expect(result).toBeFalsy()
   })
   // checkOfficeHours
   it('checkOfficeHours with valid time', async () => {
-    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const date: Date = new Date('2024-04-10T12:00:00+0800')
     const nowHour: number = date.getHours()
     const result: boolean = checkOfficeHours(nowHour, '9', '22')
 
     expect(result).toBeTruthy()
   })
   it('checkOfficeHours with after office hours', async () => {
-    const date: Date = new Date('2024-04-10T23:00:00.0800')
+    const date: Date = new Date('2024-04-10T23:00:00+0800')
     const nowHour: number = date.getHours()
     const result: boolean = checkOfficeHours(nowHour, '9', '22')
 
     expect(result).toBeFalsy()
   })
   it('checkOfficeHours with before office hours', async () => {
-    const date: Date = new Date('2024-04-10T04:00:00.0800')
+    const date: Date = new Date('2024-04-10T04:00:00+0800')
     const nowHour: number = date.getHours()
     const result: boolean = checkOfficeHours(nowHour, '9', '22')
 
     expect(result).toBeFalsy()
   })
   it('checkOfficeHours with just at end office hours', async () => {
-    const date: Date = new Date('2024-04-10T22:00:00.0800')
+    const date: Date = new Date('2024-04-10T22:00:00+0800')
     const nowHour: number = date.getHours()
     const result: boolean = checkOfficeHours(nowHour, '9', '22')
 
@@ -51,7 +51,7 @@ describe('check.ts', () => {
   })
   // checkWeekend
   it('checkWeekend with weekday', async () => {
-    const date: Date = new Date('2024-04-10T22:00:00.0800')
+    const date: Date = new Date('2024-04-10T22:00:00+0800')
     const dayOfWeek: string = date.toLocaleDateString('en-US', {
       weekday: 'long'
     })
@@ -61,7 +61,7 @@ describe('check.ts', () => {
     expect(result).toBeFalsy()
   })
   it('checkWeekend with weekend', async () => {
-    const date: Date = new Date('2024-04-12T22:00:00.0800')
+    const date: Date = new Date('2024-04-12T22:00:00+0800')
     const dayOfWeek: string = date.toLocaleDateString('en-US', {
       timeZone: 'America/Los_Angeles',
       weekday: 'long'
@@ -73,7 +73,7 @@ describe('check.ts', () => {
   })
   // convertTZ
   it('convertDateTz with valid timezone', async () => {
-    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const date: Date = new Date('2024-04-10T12:00:00+0800')
     const [resultDate, resultHour, resultDayOfWeek] = convertDateTz(date, '+8')
 
     expect(resultDate.getTime()).toEqual(date.getTime())
@@ -81,7 +81,7 @@ describe('check.ts', () => {
     expect(resultDayOfWeek.includes('wednesday')).toBeTruthy()
   })
   it('convertDateTz with different timezone', async () => {
-    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const date: Date = new Date('2024-04-10T12:00:00+0800')
     const [resultDate, resultHour, resultDayOfWeek] = convertDateTz(date, '-5')
 
     expect(resultDate.getTime()).toBeLessThan(date.getTime())
@@ -89,7 +89,7 @@ describe('check.ts', () => {
     expect(resultDayOfWeek.includes('tuesday')).toBeTruthy()
   })
   it('convertDateTz with invalid offset', async () => {
-    const date: Date = new Date('2024-04-10T12:00:00.0800')
+    const date: Date = new Date('2024-04-10T12:00:00+0800')
     const t = (): boolean => {
       convertDateTz(date, 'abc')
       return true
--- dist/index.js
+++ dist/index.js
@@ -36137,11 +36137,35 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.checkHolidays = exports.checkOfficeHours = exports.checkWeekend = exports.convertDateTz = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 const date_holidays_1 = __importDefault(__nccwpck_require__(740));
 /**
  * The convertDateTz cnnvert the date to the specified timezone
@@ -36173,6 +36197,7 @@ function convertDateTz(today, offset) {
     ];
     const resultDayOfWeekInt = resultDate.getDay();
     const resultDayOfWeek = weekday[resultDayOfWeekInt];
+    core.info(`convertDateTz returns [${resultDate}, ${resultHour}, ${resultDayOfWeek}]`);
     return [resultDate, resultHour, resultDayOfWeek];
 }
 exports.convertDateTz = convertDateTz;
